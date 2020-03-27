658aca70f98e20a9386f472b4c8964ba7848238c
Pierreauger <pierre.auger@isen-ouest.yncrea.fr>
2020-03-27 16:42:25 +0100

    dsdf

---
+++ tab.js
@@ -0,0 +1,18 @@
+function sum(numbers){
+    let s = 0;
+  for(let i = 0; i< numbers.length; i++){
+    let n = numbers[i];
+    s = n + s;
+  } 
+  return s;
+}
+sum2(numbers){
+    let result = 0;
+    for(number of numbers){
+        result = number + result;
+    }
+    return result;
+}
+function longestWord(words){
+    
+}
---
+++ tabTest.js
@@ -0,0 +1,8 @@
+function tabTest(){
+    console.log( sum([]) ); // => 0
+    console.log( sum([42,404,1337]) ); // => 1783
+    console.log( sum2([]) ); // => 0
+    console.log( sum2([42,404,1337]) ); // => 1783
+    console.log( longestWord(["lot", "of", "words", "of", "different", "sizes"]) ); // => different 
+
+}
